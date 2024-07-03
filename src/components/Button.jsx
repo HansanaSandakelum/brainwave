cbc000143a38372fa74d397f9cbe8a4f2a3b6954
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-07-03 16:21:48 +0530

    button component and link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
-const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative inline-flex item-center justify-center h-11 transition-colors hover:text-color-1 ${
+const Button = ({ className, href, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
@@ -13,7 +13,14 @@ const Button = ({ className, href, onClick, children, px, white }) => {
       {ButtonSvg(white)}
     </button>
   );
-  return renderButton();
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
